6064103b42e57388b098e5ec1f0cf36eb679c5dc
Daniel Avelino <daniel_avelino@outlook.com>
2021-07-04 11:25:50 -0300

    navbar adjusts

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,32 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary main-nav">
-      <div className="container-fluid"> 
+    <nav className="navbar navbar-expand-sm navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
         <a href="link" className="nav-title">
           <h4>Covid</h4>
         </a>
-        <div className = "collapse navbar-collapse">
-          <ul className = "navbar-nav offset-sm-2 main-menu">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#toggler-navbar"
+          aria-controls="toggler-navbar"
+          aria-expanded="false"
+          aria-label="toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="toggler-navbar">
+          <ul className="navbar-nav offset-sm-2 main-menu">
             <li>
-              <a href="link" className="selected">HOME</a>
+              <a href="link" className="selected">
+                HOME
+              </a>
             </li>
             <li>
               <a href="link">CASES</a>
